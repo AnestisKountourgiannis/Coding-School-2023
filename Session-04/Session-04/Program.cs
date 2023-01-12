023baf08548aabda7fa45aabac4dda9bda19295b
Anestis Kountourgiannis <anekoujohn@gmail.com>
2023-01-12 14:31:36 +0200

    pr cl

--- Session-04/Session-04/Program.cs
+++ Session-04/Session-04/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// 1. Write a C# program to print Hello and your name in the same line.
+string hello = "Hello";
+string name = " Anestis";
+
+Console.WriteLine(hello + name);
+
+//2.Write a C# program to print the sum of two numbers and the division of those two numbers.
+
