c4b5b1b0a4c163ecdc5b307a48fdc5127f750448
Anestis Kountourgiannis <anekoujohn@gmail.com>
2023-01-16 14:51:24 +0200

    rewrite

--- Session-06/Session-06/Session-06/Session-06/Person.cs
+++ Session-06/Session-06/Session-06/Session-06/Person.cs
@@ -1,7 +1,40 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
 
-using Session_06;
-using System.Net.Http.Headers;
-
-
-
+namespace Session_06
+{
+   //properties
+    public class Person
+    {
+        public Guid ID { get; set; }
+        public string? Name { get; set; }
+        public int Age { get; set; }
+    }
+    //constructors
+    public Person() {
+    }
+    public Person(Guid id) {
+        ID = id;
+    }
+    public Person(Guid id, string name) {
+        ID = id;
+        Name = name;
+    } 
+    public Person(Guid id,string name,int age)
+    {
+        ID = id;
+        Name = name;
+        Age = age;
+    }
+//methods
+public void GetName() {
+    }
+public void SetName(string name) {
+    }
+}
---
+++ Session-06/Session-06/Session-06/Session-06/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Session_06;
+using System.Net.Http.Headers;
+
+
+
