03fd063038daa261013abea30554cb1c9cdaa825
Anestis Kountourgiannis <anekoujohn@gmail.com>
2023-01-16 17:45:56 +0200

    inst

---
+++ Session-06/Session-06/Session-06/Session-06/Institute.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    public class Institute
+    {
+        public Guid ID { get; set; }
+        public string? Name { get; set; }
+        public int YearsInService { get; set; }
+    }
+    //constructors
+    public Institute()
+    {
+
+    }
+    public Institute(Guid id)
+    {
+        ID = id;
+    }
+    public Institute(Guid id, string name)
+    {
+        ID = id;
+        Name = name;
+    }
+    public Institute(Guid id, string name, int age)
+    {
+        ID = id;
+        Name = name;
+        YearsInService = age;
+    }
+    //Methods
+    public void GetName()
+    {
+
+    }
+    public void SetName(string name)
+    {
+
+    }
+}
--- Session-06/Session-06/Session-06/Session-06/Student.cs
+++ Session-06/Session-06/Session-06/Session-06/Student.cs
@@ -8,12 +8,12 @@ using System.Threading.Tasks;
 
 namespace Session_06
 {
-    internal  class Student : Person
+    internal class Student : Person
     {
         public int RegistrationNumber { get; set; }
         public Course[] Courses;
 
-        public void Attend(Course course,DateTime datetime) { }
-        public void WriteExam(Course course,DateTime datetime) { } 
-
-      
+        public void Attend(Course course, DateTime datetime) { }
+        public void WriteExam(Course course, DateTime datetime) { }
+    }
+}
