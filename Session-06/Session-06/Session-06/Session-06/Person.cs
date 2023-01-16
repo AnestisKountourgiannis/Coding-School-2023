113c4e846064d84eb8ce3e0761896984f6f3ba2e
Anestis Kountourgiannis <anekoujohn@gmail.com>
2023-01-16 17:50:42 +0200

    Univer

--- Session-06/Session-06/Session-06/Session-06/Person.cs
+++ Session-06/Session-06/Session-06/Session-06/Person.cs
@@ -3,38 +3,54 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
-using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Session_06
 {
-    //properties
     public class Person
     {
+
+        // properties
         public Guid ID { get; set; }
         public string? Name { get; set; }
         public int Age { get; set; }
-    }
-    //constructors
-    public Person() {
-    }
-    public Person(Guid id) {
-        ID = id;
-    }
-    public Person(Guid id, string name) {
-        ID = id;
-        Name = name;
-    } 
-    public Person(Guid id,string name,int age)
-    {
-        ID = id;
-        Name = name;
-        Age = age;
-    }
-//methods
-public void GetName() {
-    }
-public void SetName(string name) {
+
+        // constructors
+        public Person()
+        {
+
+        }
+
+        public Person(Guid id)
+        {
+            Guid ID = id;
+        }
+
+        public Person(Guid id, string name)
+        {
+           Guid  ID = id;
+            string Name = name;
+        }
+
+        public Person(Guid id, string name, int age)
+        {
+            Guid ID = id;
+           string Name = name;
+           int Age = age;
+        }
+
+        // methods
+        public void GetName()
+        {
+
+        }
+
+        public void SetName(string name)
+        {
+
+
+        }
+
     }
 }
