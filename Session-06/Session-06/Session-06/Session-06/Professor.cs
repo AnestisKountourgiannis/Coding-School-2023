0744920ca23eda5c15be55e499ac519d633036b3
Anestis Kountourgiannis <anekoujohn@gmail.com>
2023-01-16 14:59:14 +0200

    lets see

---
+++ Session-06/Session-06/Session-06/Session-06/Professor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Session_06
+{
+    public partial class Professor : Component
+    {
+        public Professor()
+        {
+            InitializeComponent();
+        }
+
+        public Professor(IContainer container)
+        {
+            container.Add(this);
+
+            InitializeComponent();
+        }
+    }
+}
