65116644447458003ef802b94d0aeb41fc272727
Anestis Kountourgiannis <anekoujohn@gmail.com>
2023-01-16 16:47:09 +0200

    grade

--- Session-06/Session-06/Session-06/Session-06/Grade.cs
+++ Session-06/Session-06/Session-06/Session-06/Grade.cs
@@ -3,27 +3,42 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Session_06
-{  
-    public class Grade 
+{
+    public class Grade
     {
         //properties
-        public Guid ID { get; set; } 
+        public Guid ID { get; set; }
         public Guid StudentID { get; set; }
-        public Guid CourseID { get; set; } 
-        public int Grade { get; set; }
+        public Guid CourseID { get; set; }
+        public int Grades { get; set; }
         //constructors
         public Grade() { }
         public Grade(Guid id)
         {
             ID = id;
         }
-        public Grade(Guid ID, Guid StudentID)
+        public Grade(Guid id, Guid studentid)
         {
-            ID =    
+            ID = id;
+            StudentID = studentid;
         }
-
-
+        public Grade(Guid id,Guid studentid,Guid courseid)
+        {
+                ID = id;
+                StudentID= studentid;
+                CourseID = courseid;
+        }
+        public Grade(Guid id, Guid studentid, Guid courseid, int grade)
+        {
+            ID = id;
+            StudentID = studentid;
+            CourseID = courseid;
+            Grades= grade;
+        }
+    }
+}
