785865ae8d33ce7cec2043826bd153192cb53523
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-01-17 17:51:14 +0200

    classes

--- Session-07/Session-07/Program.cs
+++ Session-07/Session-07/Program.cs
@@ -1,2 +1,102 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Session_07
+{
+    public enum ActionEnum
+    {
+        Convert,
+        Uppercase,
+        Reverse
+    }
+    public class ActionRequest
+    {
+
+        //properties
+        public Guid RequestID { get; set; }
+        public string? Input { get; set; }
+        public ActionEnum Action { get; set; }
+        //constructors
+        public ActionRequest() {}
+    public ActionRequest(Guid requestid)
+        {
+        RequestID = requestid;
+        }
+public ActionRequest(Guid requestid,string input)
+        {
+            RequestID = requestid;
+            Input = input;
+        }
+
+    public ActtionRequest(Guid requestid, string? input, ActionEnum action)
+        {
+        RequestID = requestid;
+        Input = input;
+        Action = action;
+        }
+    }
+    public class Message
+    {
+      public Guid ID { get; set; }
+      public DateTime TimeStamp { get; set; }
+      public string? Message { get; set; }
+        public Message() {}
+        public Message (Guid id)
+        {
+            ID = id;
+        }
+        public Message (Guid id, DateTime timestamp)
+        {
+            ID = id;
+            TimeStamp = timestamp;
+        }
+        public Message(Guid id, DateTime timeStamp, string? message)
+        {
+            ID = id;
+            TimeStamp = timeStamp;
+            Message = message;
+        }
+    }
+    public class MessageLogger
+    {
+        public Message[]? Messages { get; set; }
+
+        public void ReadAll() {
+        }
+        public void Clear() {
+        }
+        public void Write(Message) {
+        }
+    }
+    public class ActionResponse
+    {
+        public Guid RequestID { get; set; }
+        public Guid ResponseID { get; set; }
+        public string? Output { get; set; }
+        public ActionResponse() { }
+        public ActionResponse(Guid requestid)
+        {
+            RequestID = requestid;
+        }
+        public ActionResponse(Guid requestid, Guid responseid)
+        {
+            RequestID = requestid;
+            ResponseID = responseid;
+        }
+        public ActionResponse(Guid requestid, Guid responseid, string? output)
+        {
+            RequestID = requestid;
+            ResponseID = responseid;
+            Output = output;
+        }
+    } 
+    public class ActionResolver
+    {
+        public MessageLogger Logger { get; set; }
+        public ActionResolver()
+        {
+        }
+        public ActionResponse Execute(ActionRequest requestid)
+        {
+            return null;
+        }
+    }
+}
