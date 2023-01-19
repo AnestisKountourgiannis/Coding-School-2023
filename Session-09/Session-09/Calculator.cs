c29a0d75981c0e2262c3b892ff9305a7bf260e21
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-01-19 18:04:07 +0200

    started late,out of time,continuing (change)

--- Session-09/Session-09/Calculator.cs
+++ Session-09/Session-09/Calculator.cs
@@ -5,23 +5,80 @@ namespace Session_09
         private decimal? _value1 = null;
         private decimal? _value2 = null;
         private decimal? _result = null;
+        private CalcOperation _calcOperation;
+        enum CalcOperation
+        {
+            Addition
+        }
         public Calculator()
         {
             InitializeComponent();
         }
 
-        private void Zerobutton_Click(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e)
         {
+
+        }
+        private void Equalbutton_Click(object sender, EventArgs e)
+        {
+            ctrlDisplay.Text += " = ";
+
+            switch (_calcOperation)
             {
-                if (TxtBox.Text == "0")
-                {
-                    TtxBox.Text = "0";
-                }
-                else
-                {
-                    txtBox.Text += "0";
-                }
-            } 
+                case CalcOperation.Addition:
+
+                    Addition addition = new Addition();
+                    _result = addition.Do(_value1, _value2);
+
+                    break;
+
+                default:
+                    break;
+            }
+            ctrlDisplay.Text += _result;
+        }
+        private void btnOne_Click(object sender, EventArgs e)
+        {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+            ctrlDisplay.Text += " 1 ";
+            if (_value1 == null)
+            {
+                _value1 = 1;
+            }
+            else
+            {
+                _value2 = 1;
+            }
+        }
+        private void Additionbutton_Click(object sender, EventArgs e)
+        {
+            ctrlDisplay.Text += " + ";
+            _calcOperation = CalcOperation.Addition;
+        }
+        private void Twobutton_Click(object sender, EventArgs e)
+        {
+            if (_result != null)
+            {
+                ctrlDisplay.Text = string.Empty;
+                _value1 = null;
+                _value2 = null;
+                _result = null;
+            }
+            ctrlDisplay.Text += " 2 ";
+            if (_value1 == null)
+            {
+                _value1 = 2;
+            }
+            else
+            {
+                _value2 = 2;
+            }
         }
     }
 }
