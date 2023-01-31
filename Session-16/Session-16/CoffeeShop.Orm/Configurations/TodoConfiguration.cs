b62c602e02c05f6ee1e534a4c6d3e96ad15881ce
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-01-31 16:15:43 +0200

    chan

---
+++ Session-16/Session-16/CoffeeShop.Orm/Configurations/TodoConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using ClassLibrary1;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeeShop.Orm.Configurations
+{
+    public class TodoConfiguration : IEntityTypeConfiguration<Todo>
+    {
+        public void Configure(EntityTypeBuilder<Todo> builder)
+        {
+            builder.ToTable("Todo");
+            builder.HasKey(todo => todo.Id);
+            builder.Property(todo => todo.Title).HasMaxLength(50);
+        }
+    }
+}
--- Session-16/Session-16/CoffeeShop.Orm/Context/TodoContext.cs
+++ Session-16/Session-16/CoffeeShop.Orm/Context/TodoContext.cs
@@ -1,23 +1,26 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using ClassLibrary1;
+using CoffeeShop.Orm.Configurations;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace CoffeeShop.Orm
+namespace CoffeeShop.Orm.Context
 {
     public class TodoContext : DbContext
     {
-        public DbSet<Todo> Todos {get; set;}
+        public DbSet<Todo> Todos { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.ApplyConfiguration(new TodoConfiguration());
             base.OnModelCreating(modelBuilder);
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=TodoDb;Integrated Security=True;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;");
-                base.OnConfiguring(optionsBuilder);
+            base.OnConfiguring(optionsBuilder);
         }
     }
 }
