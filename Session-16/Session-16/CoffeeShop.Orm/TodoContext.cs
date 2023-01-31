33d9830bec9a965123e12768df9ef07692ffe953
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-01-31 14:38:54 +0200

    save

--- Session-16/Session-16/CoffeeShop.Orm/TodoContext.cs
+++ Session-16/Session-16/CoffeeShop.Orm/TodoContext.cs
@@ -9,6 +9,15 @@ namespace CoffeeShop.Orm
 {
     public class TodoContext : DbContext
     {
-        public DbSet<Todo> Todos {get; set;}  
+        public DbSet<Todo> Todos {get; set;}
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("");
+                base.OnConfiguring(optionsBuilder);
+        }
     }
 }
