ff638d7a4d54b33102fe5c1393a554a52a03c3ee
AnestisKountourgiannis <Anekoujohn@gmail.com>
2023-02-13 01:43:07 +0200

    create sess and adds

---
+++ Session-23/CoffeeShop.MVC/Models/Employee/EmployeeDetailsDto.cs
@@ -0,0 +1,13 @@
+﻿using CoffeeShop.Model;
+using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.MVC.Models.Employee {
+    public class EmployeeDetailsDto {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Surname { get; set; }
+        public int SalaryPerMonth { get; set; }
+        public EmployeeType EmployeeType { get; set; }
+        public List<Transaction> Transactions { get; set; } =new List<Transaction>();
+    }
+}
---
+++ Session-23/CoffeeShop.MVC/Models/ProductCategory/ProductCategoryDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿using CoffeeShop.Model;
+using CoffeeShop.Model.Enums;
+
+namespace CoffeeShop.MVC.Models.ProductCategory {
+    public class ProductCategoryDetailsDto {
+        public int Id { get; set; }
+        public string Code { get; set; } = null!;
+        public string Description { get; set; } = null!;
+        public ProductType ProductType { get; set; }
+
+    }
+}
